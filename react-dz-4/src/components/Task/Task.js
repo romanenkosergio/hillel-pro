db9b7e47f852e37cef8cad49d80015dfcf8fefc8
karinamiheenko <karina.mi.08.08@gmail.com>
2022-10-06 13:18:11 +0300

    Fixed tasks

--- react-dz-4/src/components/Task/Task.js
+++ react-dz-4/src/components/Task/Task.js
@@ -8,8 +8,8 @@ const Task = ({task}) => {
 
   return (
     <li className="list">
-      <input type="checkbox" name={task.id} value={task.isChecked} onChange={() => dispatch(selectTask(task))}/>
-      <label style={task.isChecked ? {textDecoration: 'line-through'} : {}} htmlFor={task.id}>{task.title}</label>
+      <input type="checkbox" name={task.id} checked={task.completed} onChange={() => dispatch(selectTask(task))}/>
+      <label style={task.completed ? {textDecoration: 'line-through'} : {}} htmlFor={task.id}>{task.title}</label>
       <i className="bi bi-trash3-fill" onClick={() => dispatch(removeTask(task))}></i>
     </li>
   )
